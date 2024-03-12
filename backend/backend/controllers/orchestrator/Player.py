d5fb04d0a7ca819cf578f57d06647687320c9112
Marco Buratto <m.buratto@lumit.it>
2024-03-12 19:07:27 +0100

    Controllers refactoring

--- backend/backend/controllers/orchestrator/Player.py
+++ backend/backend/controllers/orchestrator/Player.py
@@ -1,46 +1,24 @@
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework import status
 
 from backend.models.Player import Player
 
-from backend.serializers.Player import PlayerSerializer as Serializer
+from backend.serializers.Player import PlayerSerializer
 
-from backend.controllers.CustomController import CustomController
-from backend.helpers.Conditional import Conditional
-from backend.helpers.Log import Log
+from backend.controllers.CustomControllerItem import CustomControllerItem
 
 
-class PlayerController(CustomController):
-    @staticmethod
-    def get(request: Request, playerId: int) -> Response:
-        loadGroup = False
-        if "loadGroup" in request.GET:
-            loadGroup = True
+class PlayerController(CustomControllerItem):
+    def __init__(self, *args, **kwargs):
+        super().__init__(subject="player", *args, **kwargs)
 
-        try:
-            Log.log("Player information")
-            data = {
-                "data": CustomController.validate(
-                    Player(id=playerId, loadGroup=loadGroup).repr(),
-                    Serializer,
-                    "value"
-                )
-            }
 
-            # Check the response's ETag validity (against client request).
-            conditional = Conditional(request)
-            etagCondition = conditional.responseEtagFreshnessAgainstRequest(data["data"])
-            if etagCondition["state"] == "fresh":
-                data = None
-                httpStatus = status.HTTP_304_NOT_MODIFIED
-            else:
-                httpStatus = status.HTTP_200_OK
-        except Exception as e:
-            data, httpStatus, headers = CustomController.exceptionHandler(e)
-            return Response(data, status=httpStatus, headers=headers)
 
-        return Response(data, status=httpStatus, headers={
-            "ETag": etagCondition["responseEtag"],
-            "Cache-Control": "must-revalidate"
-        })
+    def get(self, request: Request, playerId: int) -> Response:
+        def actionCall(**kwargs):
+            return Player(
+                id=kwargs.get("id"),
+                loadGroup=bool("loadGroup" in request.GET)
+            ).repr()
+
+        return self.getItem(request=request, actionCall=actionCall, objectId=playerId, serializer=PlayerSerializer)
--- backend/backend/controllers/orchestrator/Players.py
+++ backend/backend/controllers/orchestrator/Players.py
@@ -1,88 +1,44 @@
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework import status
 
 from backend.models.Player import Player
 
 from backend.serializers.Player import PlayerSerializer
 from backend.serializers.Players import PlayersSerializer
 
-from backend.controllers.CustomController import CustomController
-from backend.helpers.Conditional import Conditional
+from backend.controllers.CustomControllerItems import CustomControllerItems
 from backend.helpers.Cryptography import Cryptography
-from backend.helpers.Log import Log
 
 
-class PlayersController(CustomController):
-    @staticmethod
-    def get(request: Request) -> Response:
-        loadGroup = False
-        if "loadGroup" in request.GET:
-            loadGroup = True
+class PlayersController(CustomControllerItems):
+    def __init__(self, *args, **kwargs):
+        super().__init__(subject="player", *args, **kwargs)
 
-        try:
-            Log.log("Players list")
-            data = {
-                "data": {
-                    "items": CustomController.validate(
-                        [r.repr() for r in Player.list(loadGroup=loadGroup)],
-                        PlayersSerializer,
-                        "list"
-                    )
-                }
+
+
+    def get(self, request: Request) -> Response:
+        def actionCall():
+            return [
+                r.repr() for r in Player.list(
+                    loadGroup=bool("loadGroup" in request.GET)
+                )
+            ]
+
+        return self.getList(request=request, actionCall=actionCall, serializer=PlayersSerializer)
+
+
+
+    def post(self, request: Request) -> Response:
+        def actionCall(**kwargs):
+            return Player.addOrUpdate(**kwargs)
+
+        # Always return Orchestrator SSH public key.
+        # Yes, dirty and unrelated, but it avoids many players' connections.
+        r = self.postItem(request=request, actionCall=actionCall, serializer=PlayerSerializer)
+        r.data = {
+            "data": {
+                "orchestrator_ssh_public_key": Cryptography.getPublicSshKey()
             }
+        }
 
-            # Check the response's ETag validity (against client request).
-            conditional = Conditional(request)
-            etagCondition = conditional.responseEtagFreshnessAgainstRequest(data["data"])
-            if etagCondition["state"] == "fresh":
-                data = None
-                httpStatus = status.HTTP_304_NOT_MODIFIED
-            else:
-                httpStatus = status.HTTP_200_OK
-        except Exception as e:
-            data, httpStatus, headers = CustomController.exceptionHandler(e)
-            return Response(data, status=httpStatus, headers=headers)
-
-        return Response(data, status=httpStatus, headers={
-            "ETag": etagCondition["responseEtag"],
-            "Cache-Control": "must-revalidate"
-        })
-
-
-
-    @staticmethod
-    def post(request: Request) -> Response:
-        try:
-            Log.log("Player addition")
-            Log.log("User data: "+str(request.data))
-
-            serializer = PlayerSerializer(data=request.data.get("data", {}))
-            if serializer.is_valid():
-                Player.addOrUpdate(serializer.validated_data)
-
-                # Always return Orchestrator SSH public key.
-                # Yes, dirty and unrelated, but it avoids many players' connections.
-                response = {
-                    "data": {
-                        "orchestrator_ssh_public_key": Cryptography.getPublicSshKey()
-                    }
-                }
-
-                httpStatus = status.HTTP_201_CREATED
-            else:
-                httpStatus = status.HTTP_400_BAD_REQUEST
-                response = {
-                    "Signage Orchestrator Backend": {
-                        "error": str(serializer.errors)
-                    }
-                }
-
-                Log.log("User data incorrect: "+str(response))
-        except Exception as e:
-            data, httpStatus, headers = CustomController.exceptionHandler(e)
-            return Response(data, status=httpStatus, headers=headers)
-
-        return Response(response, status=httpStatus, headers={
-            "Cache-Control": "no-cache"
-        })
+        return r

8422fa5014c50c0d75cd2a2ba96e52925cfd654a
Marco Buratto <m.buratto@lumit.it>
2024-03-13 11:50:34 +0100

    Controllers refactoring

--- backend/backend/controllers/orchestrator/Event.py
+++ backend/backend/controllers/orchestrator/Event.py
@@ -1,60 +1,29 @@
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework import status
 
 from backend.models.Event import Event
 
-from backend.serializers.Event import EventSerializer as Serializer
+from backend.serializers.Event import EventSerializer
 
-from backend.controllers.CustomController import CustomController
-from backend.helpers.Log import Log
+from backend.controllers.CustomControllerItem import CustomControllerItem
 
 
-class EventController(CustomController):
-    @staticmethod
-    def delete(request: Request, eventId: int) -> Response:
-        try:
-            Log.log("Event deletion")
-
-            Event(id=eventId).delete()
-
-            httpStatus = status.HTTP_200_OK
-        except Exception as e:
-            data, httpStatus, headers = CustomController.exceptionHandler(e)
-            return Response(data, status=httpStatus, headers=headers)
-
-        return Response(None, status=httpStatus, headers={
-            "Cache-Control": "no-cache"
-        })
+class EventController(CustomControllerItem):
+    def __init__(self, *args, **kwargs):
+        super().__init__(subject="event", *args, **kwargs)
 
 
 
-    @staticmethod
-    def patch(request: Request, eventId: int) -> Response:
-        response = None
+    def patch(self, request: Request, eventId: int) -> Response:
+        def actionCall(**kwargs):
+            return Event(id=kwargs.get("id")).modify(kwargs.get("data"))
 
-        try:
-            Log.log("Event modification")
-            Log.log("User data: "+str(request.data))
+        return self.modify(request=request, actionCall=actionCall, objectId=eventId, serializer=EventSerializer)
 
-            serializer = Serializer(data=request.data.get("data", {}), partial=True)
-            if serializer.is_valid():
-                Event(id=eventId).modify(serializer.validated_data)
 
-                httpStatus = status.HTTP_200_OK
-            else:
-                httpStatus = status.HTTP_400_BAD_REQUEST
-                response = {
-                    "Signage Orchestrator Backend": {
-                        "error": str(serializer.errors)
-                    }
-                }
 
-                Log.log("User data incorrect: "+str(response))
-        except Exception as e:
-            data, httpStatus, headers = CustomController.exceptionHandler(e)
-            return Response(data, status=httpStatus, headers=headers)
+    def delete(self, request: Request, eventId: int) -> Response:
+        def actionCall(**kwargs):
+            return Event(id=kwargs.get("id")).delete()
 
-        return Response(response, status=httpStatus, headers={
-            "Cache-Control": "no-cache"
-        })
+        return self.remove(request=request, actionCall=actionCall, objectId=eventId)
